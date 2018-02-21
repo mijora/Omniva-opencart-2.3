6b2cde3f416091ed39b2241a58d61e5994b7c7b2
Nerius Jok <neriejus@gmail.com>
2018-02-21 09:26:46 +0200

    initial commit

--- catalog/language/en-gb/extension/omnivalt.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Text
-$_['text_title']                               = 'Omniva lt';
-$_['text_courier']                             = 'Courier';
-$_['text_parcel_terminal']                     = 'Parcel terminal';
-$_['text_select_terminal']                     = 'Select parcel terminal';
