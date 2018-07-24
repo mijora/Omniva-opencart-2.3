7968c1314f4861cb6bd4c0451b557d0cf18de564
Nerius Jok <neriejus@gmail.com>
2018-07-24 10:12:28 +0300

    lt-lt

---
+++ admin/language/lt-lt/extension/shipping/omnivalt.php
@@ -0,0 +1,81 @@
+<?php
+// Heading
+$_['heading_title']                            = 'Omnivalt';
+
+// Text
+$_['text_shipping']                            = 'Siuntimas';
+$_['text_success']                             = 'Atlikta: redagavimas baigtas!';
+$_['text_edit']                                = 'Redaguoti Omnivalt siuntimus';
+$_['text_courier']                             = 'Kurjeris';
+$_['text_parcel_terminal']                     = 'Paštomatai';
+$_['text_sorting_center']                      = 'Siuntų centras';
+
+//Button
+$_['button_update_terminals']                  = 'Atnaujinti terminalus';
+$_['button_cancel']                            = 'Atšaukti';
+$_['text_manifest']                            = 'Manifestas';
+$_['text_extension']                           = 'Įskiepiai';
+
+
+// Entry
+$_['entry_url']                                = 'Api URL';
+$_['entry_user']                               = 'Naudotojas';
+$_['entry_password']                           = 'Slaptažodis';
+$_['entry_service']                            = 'Paslaugos';
+$_['entry_pickup_type']                        = 'Paėmimo tipas';
+$_['entry_company']                            = 'Įmonė';
+$_['entry_bankaccount']                        = 'Banko sąskaita';
+$_['entry_pickupstart']                        = 'Paėmimo laiko pradžia';
+$_['entry_pickupfinish']                       = 'Paėmimo laiko pabaiga';
+$_['entry_cod']                                = 'COD';
+$_['entry_status']                             = 'Statusas';
+$_['entry_sort_order']                         = 'Eilės nr';
+$_['entry_parcel_terminal_price']              = 'Paštomatų siuntimo kaina';
+$_['entry_courier_price']                      = 'Kurjerio siuntimo kaina';
+$_['entry_terminals']                          = 'Terminalų skaičius';
+$_['entry_sender_name']                        = 'Siuntėjo vardas';
+$_['entry_sender_address']                     = 'Siuntėjo addresas';
+$_['entry_sender_postcode']                    = 'Siuntėjo pašto kodas';
+$_['entry_sender_country_code']                = 'Siuntėjo šalies kodas (LT)';
+$_['entry_sender_city']                        = 'Siuntėjo miestas';
+$_['entry_sender_phone']                       = 'Siuntėjo telefonas';
+$_['cron_url']                                 = 'Periodiniu užduočių url';
+
+// Error
+$_['error_permission']                         = 'Įspėjimas: Neturite teisių redaguoti Omnivalt siuntimus!';
+$_['error_url']                                = 'Api URL privalomas!';
+$_['error_user']                               = 'Naudotojas privalomas!';
+$_['error_password']                           = 'Slaptažodis privalomas!';
+$_['error_required']                           = 'Laukas privalomas!';
+
+//Menu
+$_['menu_head']             = 'Omnivalt';
+$_['menu_manifest']         = 'Siuntimai';
+$_['menu_settings']         = 'Nustatymai';
+// orders
+$_['generate_labels']       = 'Generuoti Omnivalt lipdukus';
+$_['print_manifest']        = 'Spausdinti Omnivalt manifestą';
+$_['labels_count']          = 'Lipdukų skaičius';
+$_['weight']                = 'Svoris';
+$_['cod_amount']            = 'C.O.D. suma';
+$_['delivery_method']       = 'Pristatymo metodas';
+$_['renew']                 = 'Naujinti';
+
+///Manifest
+$_['text_new_orders']       = 'Nauji';
+$_['text_awaiting']         = 'Laukiantys';
+$_['text_completed']        = 'Baigti';
+$_['text_search']           = 'Paieška}';
+$_['text_manifest']         = 'Manifestas';
+$_['text_labels']           = 'Lipdukai';
+$_['text_customer']         = 'Naudotojas';
+$_['text_date']             = 'Data redagavimo';
+$_['text_total']            = 'Viso';
+$_['text_labels']           = 'Lipdukai';
+$_['text_skip_order']       = 'Praleisti užsakymą';
+$_['text_add_order']        = 'Pridėti prie manifesto';
+$_['text_tracking_num']     = 'Siuntos nr.';
+$_['generate_label']        = 'Generuoti lipdukus';
+$_['text_skipped_zero']     = 'Užsakymų nėra, praleistų užsakymų skaičius 0!!!';
+$_['text_new_zero']         = 'Užsakymų nėra, užsakymų skaičius 0!!!';
+$_['text_start_search']     = 'Užpildykite laukus pradėti paiešką.';
