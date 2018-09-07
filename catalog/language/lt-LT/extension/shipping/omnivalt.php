a8e86ad8a8cf87fc75d1f037656209fdf769fc00
Nerius Jok <neriejus@gmail.com>
2018-09-07 15:16:47 +0300

    lt-LT

---
+++ catalog/language/lt-LT/extension/shipping/omnivalt.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title']                               = 'Omniva lt';
+$_['text_courier']                             = 'Kurjeris';
+$_['text_parcel_terminal']                     = 'Omniva paštomatai';
+$_['text_select_terminal']                     = 'Pasirinkite paštomatą';
