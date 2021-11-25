4509eaeb6f76fb486696acdb70089b002167e041
Modestas Slivinskas <modestas.slivinskas@gmail.com>
2021-11-25 11:21:29 +0200

    language name file fix

--- catalog/language/lt-LT/extension/shipping/omnivalt.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Text
-$_['text_title']                               = 'Omniva lt';
-$_['text_courier']                             = 'Kurjeris';
-$_['text_parcel_terminal']                     = 'Omniva paštomatai';
-$_['text_select_terminal']                     = 'Pasirinkite paštomatą';
